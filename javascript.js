cdc39aa0e85eaadd71e31103c80743916a86afe6
Zeteny06 <blummel.zeteny@students.jedlik.eu>
2022-03-25 08:45:09 +0100

    JS++

---
+++ javascript.js
@@ -0,0 +1,24 @@
+// function getInputValue() {
+//     var inputVal = document.getElementById("info").value;
+//     alert(inputVal)
+// }
+// function getInputValue() {
+//     var inputVal = document.getElementById("info").value;
+//     if (value = Magyarország) {
+//         var str = "adadadada";
+//     } 
+//     else if  (value = Anglia) {
+//         var str ="xd";
+//     }
+//     else if (value = Amerika) {
+//         var str ="asd";
+//     }
+//     else if (value = Németország) {
+//         var str ="séjsze";
+//     }
+//     alert(value)
+// }
+function getInputValue() {
+   var inputVal = document.getElementById("info").value;
+    alert(inputVal)
+}
